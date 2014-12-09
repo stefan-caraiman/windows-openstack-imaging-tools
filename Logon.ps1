4afa63534db1f62f967e95450fa7865cfe208772
Claudiu Popa <cpopa@cloudbasesolutions.com>
2014-12-09 22:25:16 +0200

    Debug

--- Logon.ps1
+++ Logon.ps1
@@ -2,11 +2,11 @@ $ErrorActionPreference = "Stop"
 
 try
 {
-    $Host.UI.RawUI.WindowTitle = "Winrm config..."
-    $winrmLUrl = "https://raw.github.com/trobert2/windows-openstack-imaging-tools/master/SetupWinRMAccess.ps1"
-    $winrmPath = "$ENV:Temp\SetupWinRMAccess.ps1"
-    (new-object System.Net.WebClient).DownloadFile($winrmLUrl, $winrmPath)
-    powershell -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -File $winrmPath
+    # $Host.UI.RawUI.WindowTitle = "Winrm config..."
+    # $winrmLUrl = "https://raw.github.com/trobert2/windows-openstack-imaging-tools/master/SetupWinRMAccess.ps1"
+    # $winrmPath = "$ENV:Temp\SetupWinRMAccess.ps1"
+    # (new-object System.Net.WebClient).DownloadFile($winrmLUrl, $winrmPath)
+    # powershell -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -File $winrmPath
 
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
