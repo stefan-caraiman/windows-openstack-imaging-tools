448ddbc54be6a1c6564593287b0061ebb413e782
Claudiu Popa <cpopa@cloudbasesolutions.com>
2014-12-10 10:18:42 +0200

    Reenable winrm.

--- Logon.ps1
+++ Logon.ps1
@@ -2,11 +2,14 @@ $ErrorActionPreference = "Stop"
 
 try
 {
-    # $Host.UI.RawUI.WindowTitle = "Winrm config..."
-    # $winrmLUrl = "https://raw.github.com/trobert2/windows-openstack-imaging-tools/master/SetupWinRMAccess.ps1"
-    # $winrmPath = "$ENV:Temp\SetupWinRMAccess.ps1"
-    # (new-object System.Net.WebClient).DownloadFile($winrmLUrl, $winrmPath)
-    # powershell -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -File $winrmPath
+    $Host.UI.RawUI.WindowTitle = "Winrm config..."
+    $winrmLUrl = "https://raw.github.com/trobert2/windows-openstack-imaging-tools/master/SetupWinRMAccess.ps1"
+    $winrmPath = "$ENV:Temp\SetupWinRMAccess.ps1"
+    (new-object System.Net.WebClient).DownloadFile($winrmLUrl, $winrmPath)
+    powershell -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -File $winrmPath
+
+    $Host.UI.RawUI.WindowTitle = "Press any key to continue..."    
+    $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
