654dfd64b06bf03b0ebb68765d16f04989493086
Ionut Hulub <ihulub@cloudbasesolutions.com>
2015-07-07 11:22:21 +0300

    autoinstall git

--- install_git.ps1
+++ install_git.ps1
@@ -1,9 +1,4 @@
-param
-(
-    [Parameter(Mandatory=$true)]
-    [string]$gitUrl
-)
-
+$gitUrl = "https://github.com/msysgit/msysgit/releases/download/Git-1.9.5-preview20150319/Git-1.9.5-preview20150319.exe"
 $GitInstallPath = "$ENV:Temp\git-installer"
 (new-object System.Net.WebClient).DownloadFile($gitUrl, $GitInstallPath)
 cmd.exe /C call $GitInstallPath /silent
