2efe3269a16cc029db518b5cc650924247a794f0
Ionut Hulub <ihulub@cloudbasesolutions.com>
2015-07-07 11:55:11 +0300

    corrected git installer extension

--- install_git.ps1
+++ install_git.ps1
@@ -1,5 +1,5 @@
 $gitUrl = "https://github.com/msysgit/msysgit/releases/download/Git-1.9.5-preview20150319/Git-1.9.5-preview20150319.exe"
-$GitInstallPath = "$ENV:Temp\git-installer"
+$GitInstallPath = "$ENV:Temp\git-installer.exe"
 (new-object System.Net.WebClient).DownloadFile($gitUrl, $GitInstallPath)
 cmd.exe /C call $GitInstallPath /silent
 setx PATH "$env:PATH;${env:ProgramFiles(x86)}\Git\cmd;"
