c1c67fb758827dbae88b78864d186433a0e081f5
Stefan Caraiman <scaraiman@cloudbasesolutions.com>
2016-04-27 20:24:57 +0300

    Update Logon.ps1

--- Logon.ps1
+++ Logon.ps1
@@ -2,26 +2,23 @@ $ErrorActionPreference = "Stop"
 
 try
 {
-    Export-ModuleMember -Function UserSelectionList
     $Host.UI.RawUI.WindowTitle = "Winrm config..."
     $winrmLUrl = "https://raw.github.com/stefan-caraiman/windows-openstack-imaging-tools/master/SetupWinRMAccess.ps1"
     $winrmPath = "$ENV:Temp\SetupWinRMAccess.ps1"
     (new-object System.Net.WebClient).DownloadFile($winrmLUrl, $winrmPath)
     powershell -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -File $winrmPath
 
-    $Host.UI.RawUI.WindowTitle = "Press any key to continue..."
-    $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name Unattend*
     Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount
 
     $Host.UI.RawUI.WindowTitle = "Running Sysprep..."
     if([Environment]::Is64BitOperatingSystem -eq "True"){
-      $unattendXMLUrl = "https://raw.githubusercontent.com/stefan-caraiman/windows-openstack-imaging-tools/master/Unattend64.xml"
+        $unattendXMLUrl = "https://raw.githubusercontent.com/stefan-caraiman/windows-openstack-imaging-tools/master/Unattend64.xml"
+        $unattendXMLPath = "$ENV:Temp\Unattend64.xml"
     } else {
-      $unattendXMLUrl = "https://raw.githubusercontent.com/stefan-caraiman/windows-openstack-imaging-tools/master/Unattend32.xml"
+        $unattendXMLUrl = "https://raw.githubusercontent.com/stefan-caraiman/windows-openstack-imaging-tools/master/Unattend32.xml"
+        $unattendXMLPath = "$ENV:Temp\Unattend32.xml"
     }
-    $unattendXMLPath = "$ENV:Temp\Unattend.xml"
     (new-object System.Net.WebClient).DownloadFile($unattendXMLUrl, $unattendXMLPath)
     & "$ENV:SystemRoot\System32\Sysprep\Sysprep.exe" `/generalize `/oobe `/unattend:"$unattendXMLPath" `/shutdown
 }
