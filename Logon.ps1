f52ceb9b6e9e3febfa1608a1147b541a371dc807
Stefan Caraiman <scaraiman@cloudbasesolutions.com>
2016-04-27 21:56:09 +0300

    Update Logon.ps1

--- Logon.ps1
+++ Logon.ps1
@@ -4,7 +4,7 @@ try
 {
     $Host.UI.RawUI.WindowTitle = "Winrm config..."
     $winrmLUrl = "https://raw.github.com/stefan-caraiman/windows-openstack-imaging-tools/master/SetupWinRMAccess.ps1"
-    $winrmPath = "$ENV:Temp\SetupWinRMAccess.ps1"
+    $winrmPath = "$ENV:SystemRoot\Temp\SetupWinRMAccess.ps1"
     (new-object System.Net.WebClient).DownloadFile($winrmLUrl, $winrmPath)
     powershell -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -File $winrmPath
 
@@ -17,12 +17,13 @@ try
     $Host.UI.RawUI.WindowTitle = "Running Sysprep..."
     if([Environment]::Is64BitOperatingSystem -eq "True"){
         $unattendXMLUrl = "https://raw.githubusercontent.com/stefan-caraiman/windows-openstack-imaging-tools/master/Unattend64.xml"
-        $unattendXMLPath = "$ENV:Temp\Unattend64.xml"
+        $unattendXMLPath = "$ENV:SystemRoot\Temp\Unattend64.xml"
     } else {
         $unattendXMLUrl = "https://raw.githubusercontent.com/stefan-caraiman/windows-openstack-imaging-tools/master/Unattend32.xml"
-        $unattendXMLPath = "$ENV:Temp\Unattend32.xml"
+        $unattendXMLPath = "$ENV:SystemRoot\Temp\Unattend32.xml"
     }
     (new-object System.Net.WebClient).DownloadFile($unattendXMLUrl, $unattendXMLPath)
+    $unattendXMLPath
    # & "$ENV:SystemRoot\System32\Sysprep\Sysprep.exe" `/generalize `/oobe `/unattend:"$unattendXMLPath" `/shutdown
 }
 catch
