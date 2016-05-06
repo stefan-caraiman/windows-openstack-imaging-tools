0020803d966e7aba2a8f84c1696a2e9e04ed6e59
Stefan Caraiman <scaraiman@cloudbasesolutions.com>
2016-05-06 13:16:33 +0300

    Sleep after throwing an error.

--- SetupWinRMAccess.ps1
+++ SetupWinRMAccess.ps1
@@ -14,14 +14,16 @@ function ShouldInstallWinRMHttpListener() {
 
 if (ShouldInstallWinRMHttpListener) {
     & winrm create winrm/config/Listener?Address=*+Transport=HTTP `@`{Hostname=`"$($ENV:COMPUTERNAME)`"`}
-    if ($LastExitCode) { throw "Failed to setup WinRM HTTP listener" }
+    if ($LastExitCode) { throw "Failed to setup WinRM HTTP listener" Start-Sleep -s 10 }
 }
 
 & winrm set winrm/config/service `@`{AllowUnencrypted=`"true`"`}
-if ($LastExitCode) { throw "Failed to setup WinRM HTTP listener" }
+if ($LastExitCode) { throw "Failed to setup WinRM HTTP listener" Start-Sleep -s 10 }
 
 & winrm set winrm/config/service/auth `@`{Basic=`"true`"`}
-if ($LastExitCode) { throw "Failed to setup WinRM basic auth" }
+if ($LastExitCode) { throw "Failed to setup WinRM basic auth" Start-Sleep -s 10 }
 
 & netsh advfirewall firewall add rule name="WinRM HTTP" dir=in action=allow protocol=TCP localport=5985
-if ($LastExitCode) { throw "Failed to setup WinRM HTTP firewall rules" }
+if ($LastExitCode) { throw "Failed to setup WinRM HTTP firewall rules" Start-Sleep -s 10 }
+
+Start-Sleep -s 10
