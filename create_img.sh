14b01c21abebd444eead688fff794abb829405e0
Stefan Caraiman <scaraiman@cloudbasesolutions.com>
2016-06-09 15:47:32 +0300

    Fix mouse offset

--- create_img.sh
+++ create_img.sh
@@ -12,4 +12,4 @@ fi
 echo "creating disk!"
 qemu-img create -f qcow2 -o preallocation=metadata $IMAGE 17G
 echo "installing windows on disk!"
-$KVM -m 2048 -smp 2 -cdrom $ISO -drive file=$VIRTIO_ISO,index=3,media=cdrom,boot=on -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :3
+$KVM -m 2048 -smp 2 -cdrom $ISO -drive file=$VIRTIO_ISO,index=3,media=cdrom,boot=on -fda $FLOPPY $IMAGE -boot d -vga std -k en-us -vnc :3 -usbdevice tablet
